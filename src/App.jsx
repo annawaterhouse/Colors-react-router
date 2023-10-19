95869724b32d234c1a020fbeaeef58bf2b63ec7f
Arthur <arthur91021@gmail.com>
2023-10-19 09:52:28 -0700

    made routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 
 import './App.css'
-
+import {Routes, Route} from  'react-router-dom'
 function App() {
 
 
@@ -10,7 +10,12 @@ function App() {
      <div id="container">
    
       <div id="navbar">{/* navigation here */}</div>
-      <div id="main-section">{/* routes here */}</div>
+      <div id="main-section">
+        <Routes>
+          <Route path='/blue' element={<h1>Blue</h1>} />
+          <Route path='/red'  element={<h1>Red</h1>} />
+        </Routes>
+      </div>
     </div>
     </>
   )
