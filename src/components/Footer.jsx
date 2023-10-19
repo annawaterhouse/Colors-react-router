3292f1bce3284790a381aced711882896674d808
annawaterhouse <annawaterhouse05@gmail.com>
2023-10-19 15:18:41 -0400

    reorg navigation

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,10 @@ function Footer() {
     return (
         <>
             <div className="footer">
+                <Link to='/'>Home</Link>
                 <Link to='/blue'>Blue</Link>
                 <Link to='/red' >Red</Link>
                 <Link to='/green'>Green</Link>
-                <Link to='/'>Home</Link>
             </div>
         </>
     )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import {Link} from  'react-router-dom'
+
 function Navbar() {
     return (
         <>
             <div id="navbar">
-                <Link to='/blue'> Here is Blue!</Link>
-                <Link to='/red' >Here is Red! </Link>
-                <Link to='/green' >Here is Green! </Link>
                 <Link to='/'>Home</Link>
+                <Link to='/blue'>Here is Blue!</Link>
+                <Link to='/red'>Here is Red!</Link>
+                <Link to='/green'>Here is Green!</Link>
             </div>
         </>
     )
